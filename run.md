8510e44d82c0f111afb1c751b0721b91a9b4bcd7
Amit Karpe <amitkarpe@gmail.com>
2023-05-18 04:40:33 +0000

    Removed all other files

--- run.md
+++ run.md
@@ -1,76 +1,21 @@
-# Not valid instructions
+# Quick Run
 
-### Quick Run
+# Assume you had setup
 
-Module not found: Can't resolve 'magic-sdk'
+* Build 
 ```
-yarn add magic-sdk@7.0.0 @walletconnect/web3-provider @web3auth/web3auth
-yarn add magic-sdk@7.0.0 @web3auth/web3auth
-```
-
-ReactMoralisError: Provide a "appId" provided to <MoralisProvider>
-
-Need to update correct serverUrl and appId values in .env
-
-```
-cat << EOF > pages/_app.js
-import { MoralisProvider } from "react-moralis";
-import "../styles/globals.css";
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <MoralisProvider 
-       serverUrl = {process.env.NEXT_PUBLIC_MORALIS_SERVER_URL}
-       appId = {process.env.NEXT_PUBLIC_MORALIS_APPLICATION_ID}
-
-    >
-      <Component {...pageProps} />
-    </MoralisProvider>
-  );
-}
-
-export default MyApp;
-EOF
+npm run build
 ```
 
-"\"clientId\" not provided, please provide clientId"
-
-Get clientId from  [Web3Auth Client ID](https://dashboard.web3auth.io/home/web3auth)
-
+* Run/Start
 ```
-      clientId: "BItvnWxTZwRz3mjWxeNZ4VUutQjNkSWUdIqUoYq8fq-hKjEQmIxfTPfz16zvVCbTPutq3OlhYr3UcKQm83xK91Q"
+npm start
 ```
 
-# Deploy on Moralis
-
-Please update moralisApiKey and  moralisApiSecret into `.env`.
+# Setup
 
 ```
-❯ yarn build && yarn export
-yarn run v1.22.17
-$ next build
-info  - Loaded env from .env
-...
-...
-
-λ  (Server)  server-side renders at runtime (uses getInitialProps or getServerSideProps)
-○  (Static)  automatically rendered as static HTML (uses no initial props)
-
-✨  Done in 18.71s.
-yarn run v1.22.17
-$ next export
-...
-...
-info  - Copying "public" directory
-info  - Exporting (2/2)
-Export successful. Files written to "out"
-
-❯ moralis-admin-cli deploy -p out
-Following servers were found:
-(0) MF-1
-What server do you want to connect to?: 0
-Deployed successfully!
-Site is available at: https://prylzpawi05g.usemoralis.com
+npm install
+npm install -g serve
+serve -s build
 ```
-
-More about [moralis-admin-cli](https://docs.moralis.io/moralis-dapp/tools/moralis-admin-cli), which will describe about how to  deploy your dApp to be hosted on Moralis using the deploy command.
---
+++ src/SendUSDC2.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { abi } from "./abi";
+
+let USDC = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+
+const options = [
+  { label: "Invest in Focus 5 - main", address: "0x22dA1eEdeBC60C1b8c3a0c48f5C81BBE2b943dD9" },
+  { label: "Invest in Focus 10 - Bro2", address: "0xbc63219a3a5453db9ccd7096c6009c1ed4e69b45" },
+];
+
+function App() {
+  const [ethersProvider, setEthersProvider] = useState();
+  const [contract, setContract] = useState();
+  const [balance, setBalance] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [signer, setSigner] = useState();
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
+  useEffect(() => {
+    (async () => {
+      await window.ethereum.enable();
+      const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(USDC, abi, signer);
+      setEthersProvider(provider);
+      setContract(contract);
+      setSigner(signer);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (contract && signer) {
+        const connectedWalletAddress = await signer.getAddress();
+        const balance = await contract.balanceOf(connectedWalletAddress);
+        const newbalance = parseFloat(balance) / Math.pow(10, 6);
+        setBalance(newbalance.toString());
+      }
+    })();
+  }, [contract, signer]);
+
+  const send = async () => {
+    setLoading(true);
+    const tx = await contract.transfer(selectedOption.address, ethers.utils.parseUnits(amount, 6));
+    await tx.wait();
+    setLoading(false);
+  };
+
+  const handleOptionChange = (e) => {
+    const selected = options.find((option) => option.label === e.target.value);
+    setSelectedOption(selected);
+  };
+
+  return (
+    <div className="App">
+      <div>USDC Balance: {balance}</div>
+      <div>
+        <label>Investment Option: </label>
+        <select value={selectedOption.label} onChange={handleOptionChange}>
+          {options.map((option, index) => (
+            <option key={index} value={option.label}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label>USDC Amount: </label>
+        <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
+      </div>
+      <button onClick={send} disabled={loading}>
+        Send USDC
+      </button>
+    </div>
+  );
+}
+
+export default App;
